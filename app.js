c3909626d088fc36242cd65e4761603b97296f3a
nmkhans <nurmoin05@gmail.com>
2025-03-14 01:33:01 +0600

    video data fetched and displayed

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const categoryContainer = document.querySelector(
   "#category_container"
 );
+const videoContainer = document.querySelector("#video_container");
 
 function loadCategories() {
   const url =
@@ -29,16 +30,15 @@ function displayCategories(categories) {
   }
 }
 
-function handleCategoryClick(e) {
-  const categoryButtons = document.getElementsByClassName(
-    "btn btn-sm"
-  );
+function handleButtonColorChange(e) {
+  const categoryButtons =
+    document.getElementsByClassName("btn btn-sm");
 
-  for(const button of categoryButtons) {
-    if(button.classList.contains("bg-red-500")) {
+  for (const button of categoryButtons) {
+    if (button.classList.contains("bg-red-500")) {
       button.classList.remove("bg-red-500");
       button.classList.remove("text-white");
-      button.classList.add("btn-neutral")
+      button.classList.add("btn-neutral");
     }
   }
 
@@ -46,4 +46,54 @@ function handleCategoryClick(e) {
   e.target.classList.add("bg-red-500", "text-white");
 }
 
+function handleCategoryClick(e) {
+  handleButtonColorChange(e);
+}
+
+function loadVideos() {
+  const url =
+    "https://openapi.programming-hero.com/api/phero-tube/videos";
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayVideo(data.videos));
+}
+
+/*
+
+<div class="card bg-base-100 w-96 shadow-sm">
+  
+</div>
+*/
+
+function displayVideo(videos) {
+  videos.forEach((video) => {
+    const videoCard = document.createElement("div");
+    videoCard.className = "card bg-base-100 shadow-sm";
+
+    videoCard.innerHTML = `
+      <figure>
+        <img
+          src="${video.thumbnail}"
+          alt="video thumbnail"
+          class="h-[200px]"
+        />
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title">
+          Card Title
+        </h2>
+        
+        <div class="card-actions justify-end">
+          <div class="badge badge-outline">Fashion</div>
+          <div class="badge badge-outline">Products</div>
+        </div>
+      </div>
+    `;
+
+    videoContainer.appendChild(videoCard)
+  });
+}
+
 loadCategories();
+loadVideos();
